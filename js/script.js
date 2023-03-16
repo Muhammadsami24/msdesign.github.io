8a70783edc58230ea5af710958ec7ff315191dc7
Muhammadsami24 <98199059+Muhammadsami24@users.noreply.github.com>
2023-03-16 23:13:23 +0500

    ms design

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let menuBtn=document.querySelector("#menu-btn");
+let navbar=document.querySelector(".header .flex .navbar");
+
+menuBtn.onclick=()=>{
+    menuBtn.classList.toggle("fa-times");
+    navbar.classList.toggle('active');
+}
+
+menuBtn.onscroll=()=>{
+    menuBtn.classList.toggle("fa-times");
+    navbar.classList.toggle('active');
+}
+
